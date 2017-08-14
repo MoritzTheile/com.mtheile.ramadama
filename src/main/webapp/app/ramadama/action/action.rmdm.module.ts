92a8179696a17cb1639cf89ab8ce5bed3110c2ab
MoritzTheile <github.com@moritztheile.de>
2017-08-14 10:22:11 +0200

    cleaned out action-create module

--- src/main/webapp/app/ramadama/action/action.rmdm.module.ts
+++ src/main/webapp/app/ramadama/action/action.rmdm.module.ts
@@ -3,32 +3,31 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { RamadamaSharedModule } from '../../shared';
-import { ActionCreateComponent } from './action-create.rmdm.component';
 import { ActionCreateWizardComponent } from './action-create-wizard.rmdm.component';
 import { ActionListComponent } from './action-list.rmdm.component';
-import { StateCreateComponent } from './state-create.rmdm.component';
 import { StateViewComponent } from './state-view.rmdm.component';
+import { ActionCreatePopupService } from './action-create-popup.service';
+import { actionPopupRoute } from './action.rmdm.route';
 
 @NgModule({
     imports: [
         RamadamaSharedModule,
         ReactiveFormsModule,
+        RouterModule.forRoot(actionPopupRoute, { useHash: true }),
     ],
     exports: [
-        ActionCreateComponent,
         ActionCreateWizardComponent,
         ActionListComponent,
     ],
     declarations: [
-        ActionCreateComponent,
         ActionCreateWizardComponent,
-        StateCreateComponent,
         StateViewComponent,
         ActionListComponent,
     ],
     entryComponents: [
     ],
     providers: [
+        ActionCreatePopupService
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
